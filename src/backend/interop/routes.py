b8217af35fd33d7e9d6e8424753dc5c2c056ec0f
Xindi Wu <wuxindi1127@gmail.com>
2026-04-05 21:52:39 +0000

    fix entity counts when no search query by initializing with default values

--- src/backend/interop/routes.py
+++ src/backend/interop/routes.py
@@ -279,8 +279,8 @@ def api_entities():
             dt = dt.replace(tzinfo=UTC)
         return dt.astimezone(UTC).strftime("%Y-%m-%d")
 
+    result_entity_counts = {"gene": 0, "strain": 0}
     if search_query:
-        result_entity_counts = {"gene": 0, "strain": 0}
         for mapping, source_db_name, entity_type_name in result:
             entity_key = entity_type_name.lower()
             if entity_key in result_entity_counts:
